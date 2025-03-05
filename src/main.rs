1811b285f3b50b9b1e95efb9b03677e365adda51
Tom Hastjarjanto <tom@mailx.nl>
2025-03-05 21:44:40 +0100

    chore: support gitignore and print top files per extension

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ignore::gitignore::Gitignore;
 use rayon::prelude::*;
 use std::collections::HashMap;
 use std::env;
@@ -25,18 +26,43 @@ fn main() {
     let path = &args[1];
     let files = list_files(Path::new(path));
     let mut extension_map: HashMap<String, usize> = HashMap::new();
-    for file in files {
-        *extension_map.entry(file.extension).or_insert(0) += file.line_count;
+    for file in files.iter() {
+        *extension_map.entry(file.extension.clone()).or_insert(0) += file.line_count;
     }
     println!("{:<10} | {:<10}", "Extension", "Line Count");
     println!("--------------------------");
     for (extension, line_count) in extension_map {
         println!("{:<10} | {:<10}", extension, line_count);
     }
+
+    println!("");
+    // print top 5 biggest files per extension
+    let mut extension_files: HashMap<String, Vec<&FileInfo>> = HashMap::new();
+    for file in files.iter() {
+        extension_files
+            .entry(file.extension.clone())
+            .or_insert(Vec::new())
+            .push(file);
+    }
+    for (extension, files) in extension_files {
+        let mut sorted_files = files;
+        sorted_files.sort_by(|a, b| b.line_count.cmp(&a.line_count));
+        println!("Top 5 biggest files for extension: {}", extension);
+        println!("{:<10} | {:<10} | {}", "Extension", "Line Count", "Path");
+        println!("--------------------------------------------");
+        for file in sorted_files.iter().take(5) {
+            println!(
+                "{:<10} | {:<10} | {}",
+                extension, file.line_count, file.path
+            );
+        }
+        println!("");
+    }
 }
 
 fn list_files(dir: &Path) -> Vec<FileInfo> {
     let mut files = Vec::new();
+    let (gitignore, _errors) = Gitignore::new(dir.join(".gitignore"));
     if dir.is_dir() {
         match fs::read_dir(dir) {
             Ok(entries) => {
@@ -45,6 +71,9 @@ fn list_files(dir: &Path) -> Vec<FileInfo> {
                     .filter_map(|entry| {
                         if let Ok(entry) = entry {
                             let path = entry.path();
+                            if gitignore.matched(&path, path.is_dir()).is_ignore() {
+                                return None;
+                            }
                             if path.is_dir() {
                                 Some(list_files(&path))
                             } else {
